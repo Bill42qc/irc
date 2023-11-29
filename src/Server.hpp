fd73ede55b414dc99ab6207ee456fc38463ee1f7
yup339 <pascaloubergeron@hotmail.com>
2023-11-29 16:41:21 -0500

    added exception because billyLazy

--- src/Server.hpp
+++ src/Server.hpp
@@ -2,9 +2,14 @@
 
 #include <iostream>
 #include <map>
+#include <unistd.h>
 #include "Client.hpp"
 #include "Channel.hpp"
 #include "ACommand.hpp"
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <stdexcept>
 
 class Server
 {
@@ -12,6 +17,7 @@ private:
 	std::map<Client, std::string> clientMap_;
 	std::map<Channel, std::string> channelMap_;
 	std::string	password_;
+	int socket_;
 	uint32_t port_;
 public:
 	// Constructors / Destructor
@@ -23,6 +29,16 @@ public:
 	void addClient(Client &client);
 	void removeClient(const Client &client);
 	void createChannel(const std::string &name);
+
+	class SocketException : public std::exception {
+	public:
+		const char* what() const throw(){return "Error creating socket.";}
+	};
+
+	class BindingException : public std::exception {
+	public:
+		const char* what() const throw(){return "error binding the socket";}
+	};
 };
 
 // Server_HPP
