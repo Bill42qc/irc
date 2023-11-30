f82bc67668ae5fc739c15470525abfde12479787
Billy Martin <bmartin@c1r1p5.42quebec.com>
2023-11-30 18:11:53 -0500

    documentation + make weechat

--- src/Server.cpp
+++ src/Server.cpp
@@ -95,6 +95,7 @@ void Server::handleClientInput(int i){
 		std::cout << "Connection closed by client." << std::endl;
 		close(clientSocket);
 		removeClient(i);
+		pollfd_.erase(pollfd_.begin()+i+1);
 	} 
 	else {
 		throw RecvException();
